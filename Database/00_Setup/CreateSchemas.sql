f0ae70261324e606b04a643e3795c84e28000257
ruturaj0917 <jadhavruturaj99@gmail.com>
2026-07-03 20:14:13 +0530

    Initial project structure and database setup

---
+++ Database/00_Setup/CreateSchemas.sql
@@ -0,0 +1,26 @@
+USE LeaseBillingEngine;
+GO
+
+-- Security Module
+CREATE SCHEMA security;
+GO
+
+-- Property Module
+CREATE SCHEMA property;
+GO
+
+-- Leasing Module
+CREATE SCHEMA leasing;
+GO
+
+-- Billing Module
+CREATE SCHEMA billing;
+GO
+
+-- Finance Module
+CREATE SCHEMA finance;
+GO
+
+-- Audit Module
+CREATE SCHEMA audit;
+GO
